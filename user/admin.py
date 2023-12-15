a601711da7e9e866233131f0ee8f4d600c658a11
adcurse22 <orhan03233@gmail.com>
2023-12-15 10:37:21 +0600

    so far add sqlite

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category,Exercise,Nutrition,Feedback
+from .models import Category,Exercise,Nutrition,Feedback,UserManager
 admin.site.register(Category)
 admin.site.register(Exercise)
 admin.site.register(Nutrition)
